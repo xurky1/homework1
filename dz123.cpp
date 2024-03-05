13a028a52d7f2f5a3b06d34f178eea706a63acad
xurky1 <159796649+xurky1@users.noreply.github.com>
2024-03-06 02:00:52 +0300

    Add files via upload

---
+++ dz123.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    /*1
+    float R1, R2, R3;
+    cin >> R1;
+    cin >> R2;
+    cin >> R3;
+    float R0 = 1 / R1 + 1 / R2 + 1 / R3;
+    cout << "R0 = " << R0 << "\n";*/
+
+    /*2
+    const float pi = 3.14;
+    float S, L, R;
+    cout << "Введите длину окружности: ";
+    cin >> L;
+    R = L / (pi *2);
+    S = pi * R * R;
+    cout << "S = " << S;*/
+
+    /*3
+    float v, t, a;
+    cout << "Введите v:";
+    cin >> v;
+    cout << "Введите a:";
+    cin >> a;
+    cout << "Введите t:";
+    cin >> t;
+    cout << "s = " << v * t + (a * t * t) / 2*/;
+}
