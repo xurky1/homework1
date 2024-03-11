f88bc3173d0beb0f21d64197759147cf8142a0a9
xurky1 <159796649+xurky1@users.noreply.github.com>
2024-03-11 15:15:06 +0300

    Update dz1.cpp

--- dz1.cpp
+++ dz1.cpp
@@ -1,16 +1,14 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
-
-
 int main()
 {
     setlocale(LC_ALL, "rus");
     //1
-    float R1, R2, R3;
+    float R1, R2, R3, R0;
     cin >> R1;
     cin >> R2;
     cin >> R3;
-    float R0 = 1 / R1 + 1 / R2 + 1 / R3;
+    R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
     cout << "R0 = " << R0 << "\n";
 
     //2
@@ -18,7 +16,7 @@ int main()
     float S, L, R;
     cout << "Введите длину окружности: ";
     cin >> L;
-    R = L / (PI *2);
+    R = L / (PI * 2);
     S = PI * R * R;
     cout << "S = " << S;
 
