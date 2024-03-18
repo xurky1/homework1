6afb8016e2b09edabf0436fa035dc98b25419662
xurky1 <159796649+xurky1@users.noreply.github.com>
2024-03-18 13:40:37 +0300

    Add files via upload

---
+++ dz7.cpp
@@ -0,0 +1,32 @@
+﻿using namespace std;
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    const int A = 10;
+    int array[A];
+    int min, max;
+    cin >> min >> max;
+    srand(time(NULL));
+    for (int i = 0; i < A; i++) {
+        int c = min + rand() % (max - min + 1);
+        array[i] = c;
+        cout << array[i] << " ";
+        cout << endl;
+    }
+    // bubblesort
+    bool flag;
+    for (int j = 0; j < A; j++) {
+        for (int i = 0; i < A - 1 - j; i++) {
+            flag = true;
+            if (array[i] > array[i + 1]) {
+                swap(array[i], array[i + 1]);
+                flag = false;
+            }
+        }
+        if (flag) break;
+    }
+    for (int i = 0; i < A; i++) {
+        cout << array[i] << " ";
+    }
+}
