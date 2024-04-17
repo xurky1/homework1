5d9514b361a48102c6b38b6472a6813c9fb31940
xurky1 <159796649+xurky1@users.noreply.github.com>
2024-04-17 15:18:35 +0300

    Add files via upload

---
+++ dz4.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+using namespace std;
+//1
+//int main()
+//{
+//    setlocale(LC_ALL, "rus");
+//    int a;
+//    int sum = 0;
+//
+//    cout << "a: ";
+//    cin >> a;
+//
+//    for (int i = a; i <= 500; ++i) {
+//        sum += i;
+//    }
+//
+//    cout << "cумма всех целых чисел от " << a << " до 500 равна: " << sum << endl;
+//}
+
+//2
+//int main() {
+//    setlocale(LC_ALL, "rus");
+//    int x, y, res = 1;
+//
+//    cout << "введите x: ";
+//    cin >> x;
+//
+//   cout << "введите y: ";
+//    cin >> y;
+//    
+//    for (int i = 0; i < y;i++) {
+//        res *= x;
+//    }
+//
+//    cout << res;
+//}
+
+//3
+//int main() {
+//    setlocale(LC_ALL, "rus");
+//    float sum = 0;
+//
+//    for (int i = 1; i <= 1000; i++) {
+//        sum += i;
+//    }
+//
+//    float avg = sum / 1000;
+//
+//    cout << "cреднее арифметическое: " << avg << endl;
+//}
+
+//4
+//int main() {
+//    setlocale(LC_ALL, "rus");
+//    int a;
+//    cout << "введите число a (1 <= a <= 20): ";
+//    cin >> a;
+//
+//    long long res = 1;
+//    for (int i = a; i <= 20; i++) {
+//        res *= i;
+//    }
+//
+//    cout << "произведение от" << a << " до 20: " << res << endl;
+//}
+
+//5
+//int main() {
+//    setlocale(LC_ALL, "rus");
+//    int k;
+//
+//    cout << "номер варианта: ";
+//    cin >> k;
+//
+//    for (int i = 1; i <= 10; i++) {
+//        cout << k << " x " << i << " = " << k * i << endl;
+//    }
+//}
